e426d015b6741d87f88471a7c6f3d8a80a8772d5
nathan morgan <nathanandrew4626@gmail.com>
2026-04-15 00:44:10 +0930

    Update views.py

--- justice_app/views.py
+++ justice_app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Youth
 
-# Create your views here.
+def home(request):
+    youths = Youth.objects.all()
+    return render(request, 'home.html', {'youths': youths})
+
+def recommendations(request, youth_id):
+    youth = Youth.objects.get(id=youth_id)
+    programs = youth.recommend_programs()
+    return render(request, 'recommendations.html', {
+        'youth': youth,
+        'programs': programs
+    })
